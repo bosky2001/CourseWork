9b9839cd36f43eb884d39597d38d9ead7ca6f27a
Raghavesh <42346504+bosky2001@users.noreply.github.com>
2023-06-28 19:32:48 -0400

    Add files via upload

--- mjcpy/leg/leg.py
+++ mjcpy/leg/leg.py
@@ -6,12 +6,22 @@ from numpy.linalg import inv
 import os
 import matplotlib.pyplot as plt
 from enum import Enum
-
+import time
 xml_path = 'leg.xml'
-simend = 15
+simend = 30
 
 step_no = 0
 
+class Params(Enum):
+    M_hip = 5
+    M_l1 = 0.5
+    M_l2 = 0.5
+    M_toe = 0.1
+    M_total = M_hip + M_l1+ M_l2 + M_toe
+
+    l1 = 1
+    l2 = 1
+    pass
 class Index(Enum):
 
     #x = 0
@@ -19,12 +29,14 @@ class Index(Enum):
     q1 = 1
     q2 = 2
 
+
 FSM_AIR1 = 0
 FSM_STANCE1 = 1
 FSM_STANCE2 = 2
 FSM_AIR2 = 3
 
 fsm = FSM_AIR1
+mode = 0
 
 # For callback functions
 button_left = False
@@ -38,20 +50,20 @@ def forward_kinematics(model,data):
     q1 = data.qpos[1]
     q2 = data.qpos[2]
     g= 1
-    x = np.sin(q1) + np.sin(q1+q2)
+    x = Params.l1.value * np.sin(q1) + Params.l2.value * np.sin(q1+q2)
     y = 0
-    z = -np.cos(q1) - np.cos(q1+q2)
+    z = -Params.l1.value * np.cos(q1) - Params.l2.value * np.cos(q1+q2)
     return np.array([x,y,z])
 
 def foot_jacobian(model, data):
     J = np.zeros((3,2))
     q1 = data.qpos[Index.q1.value]
     q2 = data.qpos[Index.q2.value]
-    J[0,0] = (np.cos(q1)+ np.cos(q1+q2))
-    J[0,1] = np.cos(q1+q2)
+    J[0,0] =  Params.l1.value * np.cos(q1)+  Params.l2.value * np.cos(q1+q2)
+    J[0,1] =  Params.l2.value * np.cos(q1+q2)
     
-    J[2,0] = (np.sin(q1)+np.sin(q1+q2))
-    J[2,1]=  (np.sin(q1+q2))
+    J[2,0] =  Params.l1.value * np.sin(q1)+ Params.l2.value * np.sin(q1+q2)
+    J[2,1]=   Params.l2.value * np.sin(q1+q2)
     return J
 
 def pid_controller(model, data):
@@ -78,7 +90,7 @@ def pid_controller(model, data):
     # print("and")
     # print(jac_foot)
     # print("------------------------")
-    ff = -J.T @ np.array([0,0,-1.3 * 9.81])
+    ff = -J.T @ np.array([0,0,-6.5* 9.81])
     data.ctrl[0] = tau[0] - ff[1]
     data.ctrl[1] =  tau[1]- ff[2]
 
@@ -88,67 +100,110 @@ def v_flight(model,data, zdes= -2 ,vdes=0):
     X = forward_kinematics(model,data) # [x,0,z]^T
     J = foot_jacobian(model, data)
     dX = J @ data.qvel[1:3]
-    g =  np.array([0, 0, -9.81*1.1])
-    kp = 250
-    kd = 50
+    g =  np.array([0, 0, -9.81 * Params.M_total.value ])
+    kp = 150
+    kd = 20
     
-    fz = np.array([0, 0, kp*(zdes-X[2]) + kd*(vdes - dX[2])])
-    tau = J.T@( fz+ g)
-    _foot = data.xpos[3, 2]
-    print(_foot)
+    fz = np.array([10*(-X[0]) + 15*(- dX[0]), 0, kp*(zdes-X[2]) + kd*(vdes - dX[2])])
+    tau = J.T@( fz)
+    # _foot = data.xpos[3, 2]
+    # print(_foot)
     data.ctrl[0] = tau[0]
     data.ctrl[1] = tau[1]
 
 def vhad_stance(model, data):
     q1 = data.qpos[Index.q1.value]
     q2 = data.qpos[Index.q2.value]
-    z = -np.cos(q1)- np.cos(q1+q2)
-    x = np.sin(q1)+ np.sin(q1+q2)
+    z = forward_kinematics(model, data)[2]
+    x = forward_kinematics(model, data)[0]
     #zdot = data.qvel[Index.z.value]
     dX = (foot_jacobian(model, data)@data.qvel[Index.q1.value:])
-    zdot = data.qvel[0] - dX[2]
+    zdot = dX[2]
     #nominal height
     p = -1.5
-    w = 1
+    w = 23
     phi = np.arctan2( w*(p-z),-zdot)
     
-    beta = 1
-    ka = 10
+    beta = 1.5
+    ka = 48
     kd = 1
     w_i = 0 #kd*zdot*np.sin(phi)
-    fx = 1*(-x)+ 10*(-dX[0])
-    u = np.array([fx, 0, 100*(p-z)- beta*zdot -ka*np.cos(phi) + w_i])  # (w*w)*(p-z) - beta*zdot-ka*np.cos(phi) + w
+    fx = 25*(-x)+ 15*(-dX[0])
+    u = np.array([fx, 0, w*w*(p-z)- beta*zdot -ka*np.cos(phi) ])  
     J = foot_jacobian(model, data)
-    tau = J.T@(u)
+
+    g =  np.array([0, 0, -9.81 * Params.M_total.value])
+    tau = J.T@(u -g )
+    
     data.ctrl[0] = tau[0]
     data.ctrl[1] = tau[1]
-     
+
+modes = []     
+z_height = []
+
 def vhad_control(model, data):
     
-    stance_height = -1.3
-    z = forward_kinematics(model, data)[2]
-    if(z > stance_height): v_flight(model, data, -1.5)
-    else: 
-        print("kekw")
-        data.ctrl[0] = 0
-        data.ctrl[1] = 0
+    tol = 8*1e-2
+    stance_h = -1.5
+    
+    
+    #v_flight(model, data, stance_h)
+    global mode
+    
+    if mode == 0:
+ 
+        z = forward_kinematics(model, data)[2]
+        delta_z = abs(z-stance_h)
+        if delta_z >= tol:
+            
+            mode = 1
+    
+    if mode == 1:
+        
+        z = forward_kinematics(model, data)[2]
+        delta_z = abs(z-stance_h)
+        if delta_z <= tol:
+            mode = 0
+
+    modes.append(mode)
+    z_height.append(forward_kinematics(model, data)[2])
+
+    if mode == 0:
+        v_flight(model, data, stance_h)
+    
+    if mode == 1:
+        vhad_stance(model, data)
+    # def zvel():
+    #     dX = foot_jacobian(model, data) @ data.qvel[Index.q1.value:]
+    #     zdot = dX[2]
+    #     return zdot
+    # zdot = zvel()
+    # #falling
+    # if zdot < 0:
+    #     mode = 0
+    #     if zvel() > 0:
+    #         mode = 1
+    # #post 
+    # if zdot > 0:
+    #     mode = 0
+    #     if zvel() < 0:
+    #         mode = 1   
+
+    # modes.append(mode)
+    # z_height.append(zvel())
+    # if mode == 0:
+    #     v_flight(model, data, stance_h)
+    # else:
+    #     vhad_stance(model, data) 
     
     
 def init_controller(model,data):
-    data.ctrl[0] = 0
-    data.ctrl[1] = 0
-    data.qpos[1] = -0.4
-    data.qpos[2] = 0.8
+    
+    data.qpos[1] = -0.6773
+    data.qpos[2] = 1.443
     
     
 
-def set_position_servo(actuator_no, kp):
-    model.actuator_gainprm[actuator_no, 0] = kp
-    model.actuator_biasprm[actuator_no, 1] = -kp
-
-def set_velocity_servo(actuator_no, kv):
-    model.actuator_gainprm[actuator_no, 0] = kv
-    model.actuator_biasprm[actuator_no, 2] = -kv
 
 def keyboard(window, key, scancode, act, mods):
     if act == glfw.PRESS and key == glfw.KEY_BACKSPACE:
@@ -222,60 +277,14 @@ def constraint(data):
         return J@f
     else: return np.zeros(4)
 def g_force(data):
-    mhip = 1
-    mleg = 1
-    mtoe= 0.1
-    g = 9.81
-    d1 = 0.5
-    d2 = 0.125
-    q = data.qpos[2]
-    l = data.qpos[3]
-
-    return np.array([0,g*(mtoe+mhip+ mleg), g*(d2*mtoe +d1*(mleg+mtoe) +mtoe*l)
-                     *np.sin(q), -g*mtoe*np.cos(q)])
+    pass
     
 tau_prev = P_prev =  np.zeros(4)
 
 def get_C(model, data):
-    mhip = 1
-    mleg = 1
-    mtoe= 0.1
-    g = 9.81
-    d1 = 0.5
-    d2 = 0.125
-    q = data.qpos[2]
-    l = data.qpos[3]
-    qdot = data.qvel[2]
-    ldot = data.qvel[3]
-    C = np.zeros((model.nv, model.nv))
+    pass
 
-    C[0,2] = mtoe*np.cos(q)*ldot - (d2*mtoe+d1*(mleg+mtoe)+mtoe*l)*np.sin(q)*qdot
-    C[0,3] = mtoe*np.cos(q)*qdot
-    C[1,3] = mtoe*np.sin(q)*qdot
-    C[1,2] = mtoe*np.sin(q)*ldot + np.cos(q)*(d2*mtoe + d1*(mleg + mtoe) + mtoe*l) *qdot
-    C[2,2] = 0.5*(mtoe*l + mtoe*(2*(d1+d2)+l))*ldot
-    C[2,3] = 0.5*(mtoe*l + mtoe*(2*(d1+d2)+l))*qdot
-    C[3,2] = 0.5*(mtoe*l + mtoe*(2*(d1+d2)+l))*qdot
-    return C
 
-def jacobian_toe(model,data):
-    
-    d1 = 0.5
-    d2 = 0.125
-    q = data.qpos[2]
-    l = data.qpos[3]
-    qdot = data.qvel[2]
-    ldot = data.qvel[3]
-
-    J = np.zeros((3,model.nv))
-    J[0,0] = J[-1,1]=1
-    J[0,2] = np.cos(q)*(d1+d2 +l)
-    J[0,3] = np.sin(q)
-
-    J[-1,2] = np.sin(q)*(d1+d2 +l)
-    J[-1,3] = -np.cos(q)
-
-    return J
 def momentum_observer(model,data):
 
     global P_prev, tau_prev
@@ -309,7 +318,7 @@ def momentum_observer(model,data):
 
     #contact force from joint torque
     # hip_rot = np.array([[np.cos(q),0,np.sin(q)],[0,1,0],[-np.sin(q),0,np.cos(q)]])
-    J = jacobian_toe(model, data)
+    J = None
     contact = np.linalg.pinv(J.T) @ tau_d
     return contact
 
@@ -363,7 +372,7 @@ cam.distance = 5.0
 cam.lookat = np.array([0.0, 0.0, 1.5])
 
 init_controller(model,data)
-
+#v_flight(model, data, -1.5)
 #set the controller
 
 forcetorque = np.zeros(6)
@@ -375,7 +384,7 @@ jac_com = np.zeros((3, model.nv))
 
 # theta1 = []
 # theta2 = []
-z_height = []
+
 zdes = -1.5
 while not glfw.window_should_close(window):
     simstart = data.time
@@ -391,14 +400,16 @@ while not glfw.window_should_close(window):
 
     # theta1.append(data.qpos[1])
     # theta2.append(data.qpos[2])
-    z_height.append(forward_kinematics(model, data)[2])
+    
     if (data.time>=simend):
         break;
-
+    
+    #print(mode)
     # get framebuffer viewport
     viewport_width, viewport_height = glfw.get_framebuffer_size(
         window)
     viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
+
 
     # Show joint frames
     opt.flags[mj.mjtVisFlag.mjVIS_JOINT] = 1
@@ -429,6 +440,7 @@ glfw.terminate()
 # axs[1].axhline(0.8,color = 'y', linestyle = '--')
 # axs[1].set_title("Theta2")
 # plt.subplots_adjust(hspace=0.5)
+plt.plot(modes, color = 'r', linestyle = '--')
 plt.plot(z_height)
-plt.axhline(zdes,color = 'y', linestyle = '--')
+#plt.axhline(zdes,color = 'y', linestyle = '--')
 plt.show()
