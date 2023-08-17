91f6f6ab793d4a80b3e3aba3b206dfdb164c7ae0
Raghavesh <42346504+bosky2001@users.noreply.github.com>
2023-08-17 19:44:43 -0400

    Add files via upload

--- mjcpy/twist/twist_control.py
+++ mjcpy/twist/twist_control.py
@@ -102,7 +102,7 @@ button_right = False
 lastx = 0
 lasty = 0
 
-def FL_pose(model, data, leg_id, pose = np.array([0, -0.8, -1.4]) , posevel=np.array([0, 0, 0])):
+def FL_pose(model, data, leg_id, pose = np.array([0, -0.8, -1.6]) , posevel=np.array([0, 0, 0])):
     abd_des = pose[0]
     Upper_des = pose[1]
     lower_des = pose[2]
@@ -111,33 +111,49 @@ def FL_pose(model, data, leg_id, pose = np.array([0, -0.8, -1.4]) , posevel=np.a
     Uppervel_des = posevel[1]
     lowervel_des = posevel[2]
 
-    leg = Params(model, leg_id, Fixed= False)
+    leg = Params(model, leg_id, Fixed= True)
     
-    # Kp = np.array([2.5, 3.5, 1]) FOR FIXED
+    # Kp = np.array([2.5, 3.5, 1]) #FOR FIXED
     # Kd = np.array([1, 2, 1])
 
-    Kp = np.array([ 30, 30, 30]) 
-    Kd = np.array([ 1, 1, 1])
+    Kp = np.array([ 75, 45, 35]) 
+    Kd = np.array([ 1.5, 1.5, 1.5])
 
     p_error = np.array([abd_des - data.qpos[leg.abd_id], 
                         Upper_des - data.qpos[leg.Upper_id],
                         lower_des - data.qpos[leg.Lower_id]])
     
-    d_error = np.array([abdvel_des - data.qvel[leg.abd_id-1], 
-                        Uppervel_des - data.qvel[leg.Upper_id-1],
-                        lowervel_des - data.qvel[leg.Lower_id-1]])
+    d_error = np.array([abdvel_des - data.qvel[leg.abd_id], 
+                        Uppervel_des - data.qvel[leg.Upper_id],
+                        lowervel_des - data.qvel[leg.Lower_id]])
     
     
     
     u = Kp*p_error + Kd*d_error
     jac = np.zeros((3,model.nv))
     mj.mj_jacSubtreeCom(model, data, jac, leg.body_id)
-    tau = u # + jac[:,leg.jac_s:leg.jac_e].T@np.array([0, 0, -13.5*9.81])
+    tau = u
     
     data.ctrl[leg.ctrl_range[0]] = tau[0]
     data.ctrl[leg.ctrl_range[1]] = tau[1]
     data.ctrl[leg.ctrl_range[2]] = tau[2]
 
+def ad_control(model, data):
+    qhip = data.qpos[model.joint("joint_0").id]
+    qknee = data.qpos[model.joint("joint_1").id]
+    l1 = l2 = 0.206 
+    r = l1*np.cos(qhip) + l2*np.cos(qknee )
+    print(r)
+
+    
+def spine_control(model, data, qdes =  0, qdotdes = 0):
+    q = data.qpos[model.joint("spine").id]
+    qdot =  data.qvel[model.joint("spine").id]
+    Kp = 250
+    Kd = 15
+    u = Kp*(qdes-q) + Kd*(qdotdes-qdot)
+    data.ctrl[model.actuator("Spine Torque").id] = u
+    
     
 def controller(model, data):
     """
@@ -147,11 +163,12 @@ def controller(model, data):
     FL_pose(model, data, "FL")
     
     FL_pose(model, data, "RL")
-    FL_pose(model, data, "FR")
-    FL_pose(model, data, "RR")
-    data.ctrl[model.actuator("Spine Torque").id] = 8*np.sin(2*data.time)
+    FL_pose(model, data, "FR", np.array([0, -1.57, -3.14]))
+    FL_pose(model, data, "RR", np.array([0, -1.57, -3.14]))
     
+    spine_control(model, data)
     
+    ad_control(model, data)
    
 
 def init_controller(model,data):
@@ -272,10 +289,10 @@ opt.flags[mj.mjtVisFlag.mjVIS_CONTACTFORCE] = True
 # model.vis.map.force = 0.3
 
 # install GLFW mouse and keyboard callbacks
-glfw.set_key_callback(window, keyboard)
-glfw.set_cursor_pos_callback(window, mouse_move)
-glfw.set_mouse_button_callback(window, mouse_button)
-glfw.set_scroll_callback(window, scroll)
+# glfw.set_key_callback(window, keyboard)
+# glfw.set_cursor_pos_callback(window, mouse_move)
+# glfw.set_mouse_button_callback(window, mouse_button)
+# glfw.set_scroll_callback(window, scroll)
 
 cam.azimuth = 15.608063
 cam.elevation = -11.588379
@@ -290,7 +307,6 @@ forcetorque = np.zeros(6)
 contact_x = []
 contact_y = []
 contact_z = []
-M = np.zeros((model.nv,model.nv))
 
 
     
@@ -298,10 +314,11 @@ obs_x =[]
 obs_y =[]
 obs_z =[]
 last_time = 0
+
 with viewer.launch_passive(model, data) as viewer:
   # Close the viewer automatically after 30 wall-seconds.
   start = time.time()
-  while viewer.is_running() and time.time() - start < simend:
+  while viewer.is_running() : #and time.time() - start < simend
     step_start = time.time()
 
     # mj_step can be replaced with code that also evaluates
@@ -310,7 +327,8 @@ with viewer.launch_passive(model, data) as viewer:
 
     # Example modification of a viewer option: toggle contact points every two seconds.
     with viewer.lock():
-      viewer.opt.flags[mj.mjtVisFlag.mjVIS_CONTACTPOINT] = int(data.time % 2)
+    #   viewer.opt.flags[mj.mjtVisFlag.mjVIS_CONTACTPOINT] = int(data.time % 2)
+      viewer.opt.flags[mj.mjtVisFlag.mjVIS_CONTACTFORCE] = 1
 
     # Pick up changes to the physics state, apply perturbations, update options from GUI.
     viewer.sync()
